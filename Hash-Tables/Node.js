c5d7250b1b6d75ad9c45ec4f7d1e59c848391802
charcodes24 <charcodes24@gmail.com>
2021-11-29 19:01:16 -0800

    Added setNextNode method in Node.js

--- Hash-Tables/Node.js
+++ Hash-Tables/Node.js
@@ -3,6 +3,13 @@ class Node {
     this.data = data;
     this.next = null;
   }
+
+  setNextNode(node) {
+    if (!(node instanceof Node)) {
+      throw new Error("Next node must be a member of the Node class");
+    }
+    this.next = node;
+  }
 }
 
 module.exports = Node;
